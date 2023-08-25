ad9faea7b1e52b982ebe1a77e5e6dbb0a268775b
achyu <achyutpatewar@gmail.com>
2023-08-26 00:05:04 +0530

    Initial commit

---
+++ src/main/java/BookServiceInterface.java
@@ -0,0 +1,21 @@
+public class BookServiceInterface {
+    void addBook() {
+
+    }
+
+    void showAllBooks() {
+
+    }
+
+    void showAllAvailableBooks() {
+
+    }
+
+    void borrowBook() {
+
+    }
+
+    void returnBook() {
+
+    }
+}
